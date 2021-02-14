49b6f48e95f6c630aa9b7eae8d407acda7926b3b
JMoneymaker <jasmin.moneymaker@gmail.com>
2021-02-14 15:13:56 -0800

    add simple search - detail chain

--- jailscraper/jailscraper/spiders/queryspider.py
+++ jailscraper/jailscraper/spiders/queryspider.py
@@ -7,10 +7,19 @@ class QuerySpider(scrapy.Spider):
 
     def parse(self, response):
         data = {"SearchType": "0"}
-        yield scrapy.FormRequest(url=self.search_url, formdata=data, callback=self.parse_queries)
+        yield scrapy.FormRequest(
+            url=self.search_url, 
+            formdata=data, 
+            callback=self.parse_queries
+        )
 
     def parse_queries(self, response):
-        rows = response.xpath('//table/tbody/tr')
-        bookingnos = rows.xpath("td/a/@href").extract()
-        print('Booking Numbers', bookingnos)
-        
+        link_paths = response.xpath("//td/a/@href").getall()
+        for link in link_paths:
+            booking_no = link.split('/')[4]
+            page_url = f'http://www.mcso.us/PAID/Home/Booking/{booking_no}'
+            return response.follow(page_url, self.parse_page)
+
+    def parse_page(self, response):
+        swis_id = response.xpath('//tbody/tr[1]/td[2]/text()').get()
+        print('SWIS ID', swis_id)
