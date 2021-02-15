4ec05485973981d5e8c4df4f493eb1a4733af148
JMoneymaker <jasmin.moneymaker@gmail.com>
2021-02-14 16:28:52 -0800

    add working scraper/parser for all pages

--- jailscraper/jailscraper/spiders/queryspider.py
+++ jailscraper/jailscraper/spiders/queryspider.py
@@ -15,11 +15,29 @@ class QuerySpider(scrapy.Spider):
 
     def parse_queries(self, response):
         link_paths = response.xpath("//td/a/@href").getall()
+        page_links = []
         for link in link_paths:
             booking_no = link.split('/')[4]
-            page_url = f'http://www.mcso.us/PAID/Home/Booking/{booking_no}'
-            return response.follow(page_url, self.parse_page)
+            page_links.append(f'http://www.mcso.us/PAID/Home/Booking/{booking_no}')
+        
+        return response.follow_all(page_links, self.parse_page)
 
     def parse_page(self, response):
-        swis_id = response.xpath('//tbody/tr[1]/td[2]/text()').get()
-        print('SWIS ID', swis_id)
+        def extract_by_label(query):
+            return response.xpath(f'//label[contains(./text(),"{query}")]/parent::td/following-sibling::td/text()').get(default='').strip()
+        
+        yield {
+            'swis_id': extract_by_label('SWIS ID'),
+            'full_name': extract_by_label('Name'),
+            'age': extract_by_label('Age'),
+            'gender': extract_by_label('Gender'),
+            'race': extract_by_label('Race'),
+            'height': extract_by_label('Height'),
+            'weight': extract_by_label('Weight'),
+            'hair_color': extract_by_label('Hair'),
+            'eye_color': extract_by_label('Eyes'),
+            'arresting_agency': extract_by_label('Arresting Agency'),
+            'booking_date': extract_by_label('Booking Date'),
+            'assigned_facility': extract_by_label('Assigned Facility'),
+            'projected_release_date': extract_by_label('Projected Release Date')
+        }
